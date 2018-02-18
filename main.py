dd6520bb9f12bd334c3adc2070158cbc6b665a19
PontikaGupta <pontiGupta@gmail.com>
2018-02-19 01:41:09 +0530

    user's detail

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 
 response = requests.get('https://api.jsonbin.io/b/59d0f30408be13271f7df29c').json()
-APP_ACCESS_TOKEN = 'f3879afc4df64c588581bcda0f1ddec0'
+APP_ACCESS_TOKEN =  response['access_token']
 BASE_URL='https://api.instagram.com/v1/'
 
 def self_info():
@@ -10,14 +10,15 @@ def self_info():
     user_info = requests.get(request_url).json()
 
     if user_info['meta']['code'] == 200:
-        print user_info['data']['id']
-        print user_info['data']['profile_picture']
-        print user_info['data']['username']
-        print user_info['data']['bio']
-        print user_info['data']['fullname']
-        print user_info['data']['follows']
-        print user_info['data']['follows_by']
-        print user_info['data']['website']
+        print "User's Name : " + user_info['data']['username']
+        print "User's Bio : " + user_info['data']['bio']
+        #print "user Follows : " + user_info['data']['follows']
+       # print "User's Followers : " + user_info['data']['follows_by']
+        print "Website : " + user_info['data']['website']
+        print "User's id : " + user_info['data']['id']
+        print "User's Full Name : " + user_info['data']['full_name']
+        print "User's Profile Pic : " +  user_info['data']['profile_picture']
+
     else:
         print 'Status code other than 200 received!'
 
